0831266ebe80d4263bac6f0fdb9526b8a1165644
Ranjani-31 <siva165837@gmail.com>
2025-12-03 20:00:05 +0530

    features updated

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,11 +1,48 @@
-function Card({cardType, amount}){
+import { useState } from "react";
 
-    return(
-        <div>
-            <p>{cardType}</p>
-            <p>{amount}</p>
-        </div>
-    )
+import './index.css'
+function Card({ cardType, amount, setAmount }) {
+  const [isInEdit, setIsInEdit] = useState(false);
+  const [moneyInput, setMoneyInput] = useState(amount);
+  const changeAmount = (e) => {
+    if (cardType == "Income") {
+      setIsInEdit(true);
+    }
+  };
+  const onSubmition = (e) => {
+    e.preventDefault();
+    setIsInEdit(false);
+    setAmount((c) => ({
+      ...c,
+      income: moneyInput,
+    }));
+  };
+  console.log(amount);
+  return (
+    <div className={`m-3 border px-4 py-2 rounded ${cardType==='Income'? 'income':'expense'}`}>
+      <div className="d-flex  justify-content-between align-items-center w-100">
+        <p className="fs-5 fw-bold">{cardType}</p>
+        { (!isInEdit && cardType==='Income') && (
+          <button onClick={changeAmount} className="btn mx-3 fs-6">
+            <span className="material-symbols-outlined fs-5">edit</span>
+          </button>
+        )}
+      </div>
+      {isInEdit ? (
+        <form onSubmit={onSubmition}>
+          <input
+          className="form-control"
+            onChange={(e) => setMoneyInput(e.target.value)}
+            value={moneyInput}
+            placeholder="Enter Amount"
+          />
+          <input type="submit" className="form-control my-2 " />
+        </form>
+      ) : (
+        <p className="fs-4">₹ {amount}</p>
+      )}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -7,13 +7,17 @@ import Card from '../../components/Card'
 function Home(){
 
     const [expenses , setExpenses] = useState([])
+    const [amount, setAmount] = useState({
+        expense: 0,
+        income: 0
+    })
 console.log(expenses)
 return(
-    <div className='d-flex flex-column align-items-center justify-content-around'>
-      
-        <Card cardType='Expense' amount={200} />
-        <Card cardType='Income' amount={300} />
-
+    <div className='d-flex flex-column  align-items-center justify-content-around'>
+      <div className='d-flex flex-column flex-md-row align-items-center justify-content-evenly w-100 m-5'>
+        <Card cardType='Expense' amount={amount.expense} setAmount={setAmount}/>
+        <Card cardType='Income' amount={amount.income} setAmount={setAmount}/>
+</div>
         <CreateExpense setExpenses={setExpenses}/>
         <ShowExpenses expenses={expenses}/>
 
